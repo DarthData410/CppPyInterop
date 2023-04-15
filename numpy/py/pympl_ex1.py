f27ed74cd8481bbebbf611d95f57c077e2ab628c
Brandon George <darth.data410@gmail.com>
2023-04-15 13:50:44 -0500

    updates for pympl_ex1.py

--- numpy/py/pympl_ex1.py
+++ numpy/py/pympl_ex1.py
@@ -1,7 +1,7 @@
 # file: pympl_ex1.py
 # author: J. Brandon George | darth.data410@gmail.com | @pyfryday
 # contents used for testing against C++ <-> Python interop, using Python.h from within C++ apps.
-# example
+# example for generating a ndarray of numbers, then generating sin valuess of each, and plotting the two arrays
 
 import numpy as np
 import matplotlib.pyplot as plt
