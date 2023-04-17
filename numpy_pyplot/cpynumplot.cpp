d4fec706ae5d95f5b81e6e4e91a3ea9e251a7438
Brandon George <darth.data410@gmail.com>
2023-04-17 16:42:39 -0500

    docu updates

--- numpy_pyplot/cpynumplot.cpp
+++ numpy_pyplot/cpynumplot.cpp
@@ -2,62 +2,8 @@
 // author: J. Brandon George | darth.data410@gmail.com | @pyfryday
 // contents used for testing against C++ <-> Python interop, using Python.h from within C++ apps.
 // ------------------------------------------
-// Command line execution app used for interop between C++ -> Python. Enabled by Python.h header file
-// This command line app has the following commands, and usage:
-//
-// 1. -cosvec:
-//    Uses passed in string of doubles, calling Python module numpy, and creates an NDarray from
-//    doubles. Then calls numpy.cos(x) to deliver the cosine values for passed in doubles.
-//    This NDarray of cosine doubles is then converted to vector<double> in C++, and then iterated through
-//    to display in the command output in-values and cos-values back. 
-//    
-//    Example:
-//    ./cpynumplot -cos 1.1,0.97,1.3,2.4879
-//
-//    Output:
-//    ----- cpynumplot -----
-//    function: numpy.cos(x)
-//    ----------------------
-//    in-values: 1.1,0.97,1.3,2.4879
-//    cos-values: [0.453596,0.565300,0.267499,-0.793844]
-//
-// 2. -pi:
-//    Simple call, that will not operate upon any parameters. Simply loads Pythons numpy, and calls numpy.pi. 
-//    Converts from a PyObject value to a float. Using PyFloat_AsDouble(x) call.
-//
-//    Example:
-//    ./cpynumplot -pi
-//
-//    Output:
-//    ----- cpynumplot -----
-//    function: numpy.pi
-//    ------------------
-//    return: 3.141593 
-//
-// 3. -pyplot:
-//    Uses three parameters with call: start, stop & step values. Expects these to be floats. Can use *pi in
-//    order to adjust one-or-all three values by. Uses the values to load Python modules numpy and 
-//    matplotlib.pyplot. Generates NDarray values using start, stop and step. Then retrieves the sin(x) values
-//    stored in a different NDarray. Calls pyplot to plot(x,y) and show().
-//    ./cpynumplot -pyplot <start> <stop> <step> 
-//    
-//    Examples:
-//    (Example_1): ./cpynumplot -pyplot 1 11 0.2 - Plot saved: ../numpy_pyplot/saved_plots/Example_1.png
-//    (Example_2): ./cpynumplot -pyplot 1 3*pi 0.48 - Plot saved: ../numpy_pyplot/saved_plots/Example_2.png
-//
-// 4. -rand:
-//    No parameters are used when function -rand is called. Function calls numpy.random.random() and returns
-//    the randomly generated float value.
-//
-//    Example:
-//    ./cpynumplot -rand
-//
-//    Output:
-//    ----- cpynumplot -----
-//    function: numpy.random.random
-//    -----------------------------
-//    return: 0.58332
-// 
+// Command line execution app used for interop between C++ -> Python. Enabled by Python.h header file only.
+// Showcasing soecific usage of numpy and matplotlib.pyplot modules.
 // *****************************************************************************************************
 
 #include "cpynumplot.hpp"
