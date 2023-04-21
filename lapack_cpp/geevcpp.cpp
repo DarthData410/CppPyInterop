a42bbee962d93b865a986a6b63520c527a713812
Brandon George <darth.data410@gmail.com>
2023-04-21 10:19:16 -0500

    addig lapack_cpp folder/code

---
+++ lapack_cpp/geevcpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+#include <vector>
+#include <lapack.h>
+
+using namespace std;
+using namespace complex_literals;
+
+int main() {
+    LAPACK_dgeev()
+
+    return 0;
+}
